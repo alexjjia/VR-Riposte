71354508ad7ebe43951fd32f649ecf438a91db86
Alexander Jia <jayjialex@gmail.com>
2016-09-16 17:47:52 -0400

    init commit
    bloop

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+    public float speed;
+    public GameObject target;
+    private float attackRange; //the range in which the enemy can begin attacking the target.
+    // Use this for initialization
+	void Start () {
+        transform.LookAt(target.transform);
+        attackRange = 2.5f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position += transform.forward* speed * Time.deltaTime;
+    }
+
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 boundary = new Vector3(0, transform.position.y, 0);
+        transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+        if(transform.position.y < 0)
+        {
+            transform.position = transform.position - boundary;
+        }
+    }
+}
